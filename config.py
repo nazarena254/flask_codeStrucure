a4c1531a79c0ee56859071151508cfd119b85e28
Nazarena Wambura <87655908+nazarena254@users.noreply.github.com>
2022-05-12 20:52:13 +0300

    update Config class

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
 
     # API_KEY = os.environ.get('API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI= os.environ.get(DATABASE_URL) OR Line 9
+    # SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL') OR Line 9
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://localHostMachineName:databasePassword@localhost:5432/databaseName'
 
 
